e357c52ad70bfde2a717c296c1c545715d018d1b
dxk0ta <dxkotacamps@gmail.com>
2023-04-10 22:50:55 -0700

    got style sheets connected to ejs files and login page styled a bit more

--- index.js
+++ index.js
@@ -4,22 +4,21 @@ const cookieParser = require('cookie-parser')
 const db = require('./models')
 const cryptoJS = require('crypto-js')
 require('dotenv').config()
-
 // MIDDLEWARE
 app.set('view engine', 'ejs')
 app.use(cookieParser())
 app.use(express.urlencoded({extended: false}))
-
 // AUTHENTICATION MIDDLEWARE
 app.use(async (req, res, next)=>{
-    if(req.cookies.userId) {
-        const decryptedId = cryptoJS.AES.decrypt(req.cookies.userId, process.env.SECRET)
-        const decryptedIdString = decryptedId.toString(cryptoJS.enc.Utf8)
-        const user = await db.user.findByPk(decryptedIdString)
-        res.locals.user = user
-    } else res.locals.user = null
-    next()
+  if(req.cookies.userId) {
+    const decryptedId = cryptoJS.AES.decrypt(req.cookies.userId, process.env.SECRET)
+    const decryptedIdString = decryptedId.toString(cryptoJS.enc.Utf8)
+    const user = await db.user.findByPk(decryptedIdString)
+    res.locals.user = user
+  } else res.locals.user = null
+  next()
 })
+app.use(express.static(__dirname + '/public'));
 
 // CONTROLLERS
 app.use('/users', require('./controllers/users'))
