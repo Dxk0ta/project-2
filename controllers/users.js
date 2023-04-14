32b3698f9e820ee38b987418e726f4279ff0c56c
dxk0ta <dxkotacamps@gmail.com>
2023-04-14 10:18:13 -0700

    fixing users.js removing gif query

--- controllers/users.js
+++ controllers/users.js
@@ -74,14 +74,6 @@ router.get('/profile', async (req, res)=>{
         const { keywords } = req.query;
 		// otherwise, show them their profile
         try {
-        // Query database to search for GIFs based on keywords
-            const gifs = await db.gif.findAll({
-            where: {
-            title: {
-            [Sequelize.Op.like]: `%${keywords}%`,
-            },
-            },
-        });
             // Fetch posts data from the database using Sequelize
             const posts = await db.post.findAll();
             // Render the "posts.ejs" template with the fetched posts data
@@ -90,8 +82,7 @@ router.get('/profile', async (req, res)=>{
             res.render('posts/posts.ejs', { posts });
             res.render('users/profile.ejs', { user: res.locals.user })
         } catch (error) {
-             // Handle error
-            console.error('Error searching for GIFs:', error);
+             // Handle error=
             res.status(500).json({ error: 'Internal server error' });
           }
 	}
